14b35c00a12cac428a7573184d51ce3b60888c1f
eurorondon03 <eurorondon03@gmail.com>
2022-03-17 19:54:09 -0400

    v1_senalesweb-2

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Router, Route, Link, link } from "Svelte-routing";
+	import Home from "./Home.svelte";
+</script>
+
+<Router>
+	<div>
+		<Route path="/" component={Home} />
+	</div>
+</Router>
