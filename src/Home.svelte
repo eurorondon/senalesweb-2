1973c2974064b6931aaa9b24c48f4efba36d5b08
eurorondon03 <eurorondon03@gmail.com>
2022-03-17 19:58:25 -0400

    v2_senalesweb-2

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,1 +1,7 @@
 <h1>hola</h1>
+
+<p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut alias
+    voluptates beatae saepe dolores hic voluptas totam provident explicabo
+    optio!
+</p>
